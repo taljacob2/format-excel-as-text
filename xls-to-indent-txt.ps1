573f6bbb1d0bea61ab212cc214f25062261f7c21
Tal Jacob <taljacob2@gmail.com>
2023-12-22 16:00:03 +0200

    Backup
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- xls-to-indent-txt.ps1
+++ xls-to-indent-txt.ps1
@@ -1,4 +1,16 @@
-$files = Get-ChildItem *.xls*
+function Parse-Worksheet {
+    param (
+        [parameter(mandatory)][System.__ComObject]$WorkBook,
+        [parameter()][Int32]$WorkSheetNumber = 1
+    )
+
+    $workSheet = $WorkBook.Worksheets($WorkSheetNumber)
+
+    # Write-Output $workSheet
+    Write-Output $workSheet["A1"]
+}
+
+$files = Get-ChildItem *.xls
 
 $Excel = New-Object -ComObject Excel.Application
 $Excel.visible = $false
@@ -6,13 +18,16 @@ $Excel.DisplayAlerts = $false
 
 ForEach ($file in $files) {
     Write-Output "Loading File '$($file.Name)'..."
-    $WorkBook = $Excel.Workbooks.Open($file.Fullname)
-    $Workbook.SaveAs("$($file.Fullname).txt", 42)   # xlUnicodeText
+    $workBook = $Excel.Workbooks.Open($file.Fullname)
+    
+    Parse-Worksheet -WorkBook $workBook
+
+    # $workbook.SaveAs("$($file.Fullname).txt", 42)   # xlUnicodeText
 }
 
 # cleanup
 $Excel.Quit()
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($WorkBook) | Out-Null
+# [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WorkBook) | Out-Null
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel) | Out-Null
 [System.GC]::Collect()
 [System.GC]::WaitForPendingFinalizers()
